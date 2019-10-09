769a2d2186414cd1d5590c0d7209efa35b4e0ad9
Pdean-Daen <54615795+Pdean-Daen@users.noreply.github.com>
2019-10-09 15:14:31 +0700

    Update tests.py

--- helloworld/hello/tests.py
+++ helloworld/hello/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 class SampleTest(TestCase):
 
-def test_alway_pass(self):
-        self.assertEqual(1, 1)
+        def test_alway_pass(self):
+                self.assertEqual(1, 1)
     
-def test_alway_fail(self):
-         self.assertEqual(2, 1)
+        def test_alway_fail(self):
+                 self.assertEqual(2, 1)
